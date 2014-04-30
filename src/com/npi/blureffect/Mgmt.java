37204a90e61b61dae9d1404e7fde42d7685539fe
nitin7 <nitin7@g.ucla.edu>
2014-04-30 12:28:47 -0700

    Swipeable interface between three libraries.
    Using tab layout listener.

---
+++ src/com/npi/blureffect/Mgmt.java
@@ -0,0 +1,171 @@
+package com.npi.blureffect;
+
+import java.io.File;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.AbsListView.LayoutParams;
+import java.io.File;
+import com.jeremyfeinstein.slidingmenu.lib.*;
+
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.AbsListView;
+import android.widget.AbsListView.LayoutParams;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+public class Mgmt extends Fragment{
+	
+	private static final String BLURRED_IMG_PATH = "image3.jpg";
+	private ListView mList;
+	private ImageView mBlurredImage;
+	private ImageView mNormalImage;
+	private float alpha;
+	private View headerView;
+	private static final int TOP_HEIGHT = 700;
+	private static final String tg = "nitin";
+	View rootView;
+	   @Override
+	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	            Bundle savedInstanceState) {
+		   
+		
+	 
+	        rootView = inflater.inflate(R.layout.fragment_mgmt, container, false);
+
+	        
+	        mList = (ListView) rootView.findViewById(R.id.list);
+	        mBlurredImage = (ImageView) rootView.findViewById(R.id.blurred_imagem);
+			mNormalImage = (ImageView) rootView.findViewById(R.id.normal_imagem);
+
+			// Get the screen width
+			final int screenWidth = ImageUtils.getScreenWidth(getActivity());
+
+			// Try to find the blurred image
+			final File blurredImage = new File(getActivity().getFilesDir() + BLURRED_IMG_PATH);
+			Log.d(tg, "M: "+blurredImage.exists());
+			
+			if (!blurredImage.exists()) {
+
+				// launch the progressbar in ActionBar
+				getActivity().setProgressBarIndeterminateVisibility(true);
+				Log.d(tg, "M: if");
+				new Thread(new Runnable() {
+
+					@Override
+					public void run() {
+
+						// No image found => let's generate it!
+						BitmapFactory.Options options = new BitmapFactory.Options();
+						options.inSampleSize = 2;
+						Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.image3, options);
+						Bitmap newImg = Blur.fastblur(getActivity(), image, 7);
+						ImageUtils.storeImage(newImg, blurredImage);
+						getActivity().runOnUiThread(new Runnable() {
+
+							@Override
+							public void run() {
+								updateView(screenWidth);
+
+								// And finally stop the progressbar
+								getActivity().setProgressBarIndeterminateVisibility(true);
+							}
+						});
+
+					}
+				}).start();
+
+			} else {
+
+				// The image has been found. Let's update the view
+				Log.d(tg, "M: else");
+				updateView(screenWidth);
+
+			}
+			
+			
+			
+	        
+			headerView = new View(getActivity());
+		
+			headerView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, TOP_HEIGHT));
+			String[] strings = getResources().getStringArray(R.array.list_content);
+			strings[0]= "Hours : /n Monday : 7:30 am - 11:00 pm \n Tuesday : 7:30 am - 11:00 pm \n Wednesday : 7:30 am - 11:00 pm \n Thursday 7:30 am - 11:00 pm \n Friday 7:30 am - 11:00 pm";
+	        MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(getActivity(), strings);
+			mList.addHeaderView(headerView);
+			mList.setAdapter(adapter); 
+			mList.setOnScrollListener(new OnScrollListener() {
+
+				@Override
+				public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+				}
+
+				/**
+				 * Listen to the list scroll. This is where magic happens ;)
+				 */
+				@Override
+				public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+					// Calculate the ratio between the scroll amount and the list
+					// header weight to determinate the top picture alpha
+					alpha = (float) -headerView.getTop() / (float) TOP_HEIGHT;
+					// Apply a ceil
+					if (alpha > 1) {
+						alpha = 1;
+					}
+
+					// Apply on the ImageView if needed
+					//if (mSwitch.isChecked()) {
+						mBlurredImage.setAlpha(alpha);
+					//}
+
+					// Parallax effect : we apply half the scroll amount to our
+					// three views
+					mBlurredImage.setTop(headerView.getTop() / 2);
+					mNormalImage.setTop(headerView.getTop() / 2);
+					//mBlurredImageHeader.handleScroll(headerView.getTop() / 2);
+
+				}
+			});
+			
+			
+	        //getActivity().getActionBar().hide();
+	        return rootView;
+	    }
+	   
+	   private void updateView(final int screenWidth) {
+			Bitmap bmpBlurred = BitmapFactory.decodeFile(getActivity().getFilesDir() + BLURRED_IMG_PATH);
+			bmpBlurred = Bitmap.createScaledBitmap(bmpBlurred, screenWidth, (int) (bmpBlurred.getHeight()
+					* ((float) screenWidth) / (float) bmpBlurred.getWidth()), false);
+			
+
+			mBlurredImage.setImageBitmap(bmpBlurred);
+
+			//mBlurredImageHeader.setoriginalImage(bmpBlurred);
+		}
+
+}
---
+++ src/com/npi/blureffect/SampleListFragment.java
@@ -0,0 +1,60 @@
+package com.npi.blureffect;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SampleListFragment extends ListFragment {
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.list, null);
+	}
+
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		SampleAdapter adapter = new SampleAdapter(getActivity());
+		adapter.add(new SampleItem("Home", R.drawable.ic_menu_home));
+		adapter.add(new SampleItem("Course Reserves", R.drawable.ic_menu_copy_holo_light));
+		adapter.add(new SampleItem("Hours", R.drawable.ic_menu_recent_history));
+		adapter.add(new SampleItem("Laptop Availability", R.drawable.ic_menu_help_holo_light));
+		adapter.add(new SampleItem("Contact", R.drawable.ic_menu_allfriends));
+		adapter.add(new SampleItem("Feedback", R.drawable.ic_menu_notifications));
+		adapter.add(new SampleItem("Credits", R.drawable.ic_menu_info_details));
+		adapter.add(new SampleItem("Article Search", R.drawable.ic_menu_info_details));
+		setListAdapter(adapter);
+	}
+
+	private class SampleItem {
+		public String tag;
+		public int iconRes;
+		public SampleItem(String tag, int iconRes) {
+			this.tag = tag; 
+			this.iconRes = iconRes;
+		}
+	}
+
+	public class SampleAdapter extends ArrayAdapter<SampleItem> {
+
+		public SampleAdapter(Context context) {
+			super(context, 0);
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				convertView = LayoutInflater.from(getContext()).inflate(R.layout.row, null);
+			}
+			TextView icon = (TextView) convertView.findViewById(R.id.list_value);
+			icon.setText(getItem(position).tag);
+			TextView title = (TextView) convertView.findViewById(R.id.book_year);
+			title.setText(getItem(position).tag);
+
+			return convertView;
+		}
+	}
+}
